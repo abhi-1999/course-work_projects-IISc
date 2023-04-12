35ef5dccb5e5ec9821c97cb8d89a2966e15bb5a5
abhijeettomy <abhijeettomy@iisc.ac.in>
2023-04-12 13:34:40 +0530

    small change to bur_from_start

--- Motion_planning_CP230/bur_from_start.m
+++ Motion_planning_CP230/bur_from_start.m
@@ -8,7 +8,8 @@ GBur_start = addnode(GBur_start,nodes_to_add);
 %####################IMPORTANT############################
 % root in GBur will already be there in GBur_start need to resolve this
 
-GBur_start.Nodes.XData(n+1:numnodes(GBur_start)) = GBur()  
+GBur_start.Nodes.XData(n+1:numnodes(GBur_start)) = GBur.Nodes.XData(:,1);
+GBur_start.Nodes.YData(n+1:numnodes(GBur_start)) = GBur.Nodes.YData(:,1);
 
 end
 
