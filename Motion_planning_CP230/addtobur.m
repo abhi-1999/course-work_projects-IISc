7d999f59a9474889e79e17ef350be17dd39892de
abhijeettomy <abhijeettomy@iisc.ac.in>
2023-04-13 19:21:24 +0530

    small changes to addtobur

--- Motion_planning_CP230/addtobur.m
+++ Motion_planning_CP230/addtobur.m
@@ -2,15 +2,15 @@ function newbur = addtobur(bur_tree,GBur)
 newbur = bur_tree;
 n = numnodes(newbur);
 
-nodes_to_add = numnodes(GBur);
+nodes_to_add = numnodes(GBur) - 1;
 
 newbur = addnode(newbur,nodes_to_add);
 
 
 j=2;
 for i=n+1:numnodes(newbur)
-    newbur.Nodes.XData(i) = GBur.Nodes.XData(j,1);
-    newbur.Nodes.YData(i) = GBur.Nodes.YData(j,1);
+    newbur.Nodes.XData(i) = GBur.Nodes.XData(j);
+    newbur.Nodes.YData(i) = GBur.Nodes.YData(j);
     j=j+1;
 end
 
@@ -18,14 +18,14 @@ end
 root = [GBur.Nodes.XData(1) GBur.Nodes.YData(1)];
 root_id = find_node_id(newbur,root);
 
-root_child = succesors(GBur,1);
+root_child = successors(GBur,1);
 
 %connect the edges
 for i=1:size(root_child,1)
-    next_point = [GBur.Nodes.Xdata(i) GBur.Nodes.YData(i)];
+    next_point = [GBur.Nodes.XData(root_child(i)) GBur.Nodes.YData(root_child(i))];
     next_point_id = find_node_id(newbur,next_point);
     newbur = addedge(newbur,root_id,next_point_id);
-    point_succ = successors(GBur,root_id(i));
+    point_succ = successors(GBur,root_child(i));
     while ~isempty(point_succ)
         curr_point_id  = next_point_id;
         next_point = [GBur.Nodes.XData(point_succ(1)) GBur.Nodes.YData(point_succ(1))];
